9efbb4eb6e0f66b1d070ba854d7f1c22aed1c062
sandipray63in <sandip_ray63in@yahoo.com>
2023-09-05 01:14:20 +0530

    removed unnecessary infra projects

--- WeatherForecastAPIInfra/azureDeploy.ps1
+++
@@ -1,9 +0,0 @@
-#Create a zip file with the contents of C:\Stuff\
-Compress-Archive -Path C:\Stuff -DestinationPath archive.zip
-
-# Add more files to the zip file
-# (Existing files in the zip file with the same name are replaced)
-Compress-Archive -Path C:\OtherStuff\*.txt -Update -DestinationPath archive.zip
-
-# Extract the zip file to C:\Destination\
-Expand-Archive -Path archive.zip -DestinationPath C:\Destination
